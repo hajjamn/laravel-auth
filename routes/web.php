41d337e7cb5d0ad690be4850449dda642e21be21
hajjamn <hajjamn@gmail.com>
2024-06-03 16:29:26 +0200

    ADD: migration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,7 +30,8 @@ Route::middleware(['auth', 'verified'])
 
         //register all other protected routes
         //CRUD POSTS
-    
+        Route::resource('projects', ProjectController::class);
+
     });
 
 Route::middleware('auth')->group(function () {
